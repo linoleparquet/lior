e8e64c271e73a19ce2eb32f573034469fd435e66
linoleparquet <layanilino@gmail.com>
2022-12-06 23:50:46 -0500

    ec2 vagrantfile

---
+++ ec2/README.md
@@ -0,0 +1,13 @@
+# Deploy Lior on a AWS EC2 instance
+
+This folder contains all necessary configuration to deploy Lior on a EC2 instance
+
+## Testing it: Light up a local EC2 instance thanks to Vagrant
+
+a Vagrantfile is available on this folder to simulate a EC2 instance.
+To deploy the vagrant box, open a terminal on this folder and run `vagrant up`
+
+# Deploying Lior
+
+Once a EC2 instance is available, on AWS or on your local machine, ssh into it.
+Then execute the `user-data.sh` script.
--- ec2/user-data.sh
+++ ec2/user-data.sh
@@ -1,10 +1,60 @@
+echo ' --- Install nginx and java ---'
 sudo amazon-linux-extras install -y nginx1 java-openjdk11;
-sudo rm -rf /usr/share/nginx/html/*;
-sudo curl -L https://github.com/linoleparquet/lior-frontend/releases/download/latest/dist.tar.gz -o dist.tar.gz;
+
+echo ' --- Download frontend application ---'
+curl -L https://github.com/linoleparquet/lior-frontend/releases/download/latest/dist.tar.gz -o dist.tar.gz;
 sudo tar xf dist.tar.gz;
 sudo mv dist/* /usr/share/nginx/html/;
-sudo nginx; #sudo systemctl enable nginx;
 
+echo ' --- Deploying nginx configuration ---'
+sudo bash -c 'cat <<EOF > /etc/nginx/conf.d/default.conf
+server {
+    listen 80;
+    root /usr/share/nginx/html;
 
-curl -L https://github.com/linoleparquet/lior-backend/releases/download/latest/lior-0.0.1-SNAPSHOT.jar --output app.jar
-java -jar -Dspring.profiles.active=h2 app.jar
+    location /api {
+        proxy_pass http://localhost:8080/;
+    }
+}
+EOF'
+sudo chown nginx:nginx /etc/nginx/conf.d/default.conf
+
+echo ' --- Download backend application ---'
+sudo mkdir /home/ec2-user/lior/
+sudo chown ec2-user:ec2-user /home/ec2-user/lior/
+sudo curl -L https://github.com/linoleparquet/lior-backend/releases/download/latest/lior-0.0.1-SNAPSHOT.jar --output /home/ec2-user/lior/lior.jar
+sudo chown ec2-user:ec2-user /home/ec2-user/lior/lior.jar
+
+# Define service
+sudo bash -c 'cat <<EOF > /etc/systemd/system/lior.service
+[Unit]
+Description=Lior backend service
+After=syslog.target network.target
+
+[Service]
+User=ec2-user   
+
+WorkingDirectory=/home/ec2-user/lior/
+
+ExecStart=/usr/bin/java -jar -Dspring.profiles.active=h2 /home/ec2-user/lior/lior.jar
+ExecStop=/bin/kill -15 $MAINPID
+
+SuccessExitStatus=143
+TimeoutStopSec=10
+
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+'
+
+echo ' --- Enable nginx and java service ---'
+sudo systemctl daemon-reload
+sudo systemctl start lior.service
+sudo systemctl enable lior.service 
+sudo systemctl start nginx; 
+sudo systemctl enable nginx;
+
+echo ' --- All done !!! ---'
